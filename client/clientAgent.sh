1cb850443f80f977d465adc12094c768e354cf24
arcsurf <ariel.couso@gmail.com>
2021-12-31 01:20:11 +0100

    update express project initialization

--- client/clientAgent.sh
+++ client/clientAgent.sh
@@ -3,4 +3,4 @@ ip=`curl ifconfig.me`
 host=`cat /etc/hostname`
 echo $ip
 echo $host
-curl -X POST http://35.199.102.88:3000/dns -H 'Content-Type: application/json' -d '{"ip":"'${ip}'","hostname":"'${host}'"}'
+curl -X POST http://x.x.x.x:3000/dns -H 'Content-Type: application/json' -d '{"ip":"'${ip}'","hostname":"'${host}'"}'
