ffead558963863de95281989ed8375fdfbaa2222
Ariel Couso <ariel.couso@gmail.com>
2022-01-13 07:45:04 +0100

    avaluate if file registeredIp.log exist and evalution of updated public ip address

--- client/clientAgent.sh
+++ client/clientAgent.sh
@@ -7,4 +7,21 @@ echo $ip
 echo $host
 echo $timestamp
 echo $date
-curl -X POST http://x.x.x.x:3000/dns -H 'Content-Type: application/json' -d '{"ip":"'${ip}'","hostname":"'${host}'","timestamp":"'${timestamp}'","date":"'${date}'"}'
+
+#Check if file exist
+if [ ! -f registeredIp.log ]
+then
+        touch registeredIp.log
+fi
+
+#Reading client public ip assigned and ip address into file registrered.
+while IFS= read -r registeredIp; do
+     
+  if [ $ip != $registeredIp ]; then
+    curl -X POST http://x.x.x.x:3000/dns -H 'Content-Type: application/json' -d '{"ip":"'${ip}'","hostname":"'${host}'","timestamp":"'${timestamp}'","date":"'${date}'"}'
+    echo $ip > registeredIp.log
+  else
+    echo "The Ip assigned is not updated"
+  fi
+
+done < registeredIp.log
