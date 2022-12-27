45f617dd8266cb4b36bdf4dd0f217eae93aa4688
Ariel Couso <ariel.couso@gmail.com>
2022-12-27 14:54:59 +0100

    Update index.js
    fix last new line into db.domainName config

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,40 +10,54 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/dns', function (req, res) {
-  //console.log(req.body);
+  console.log(req.body);
   res.send(req.body);
 
-  fs.readFile("/etc/bind/zones/master/YOUR_ZONE_FILENAME.DOMAIN", 'utf8', function (err,data) {
+  fs.readFile("/etc/bind/zones/master/<db.YOUR_DOMANIN>", 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
+    
   
   let dataList = data.toString().split("\n")
-  let dataListToWrite = dataList.map(item => item.includes(req.body.hostname) ? req.body.hostname+"\tIN\tA\t"+req.body.ip: item )
+  dataList.pop()
+  //console.log(dataList)
 
-  arrayToTxtFile(dataListToWrite, './db/db.temp', err => {
+  /* Your db.YOUR_DOMAIN file content must be created, and use TAB as a separatar fields between DOMAIN_NAME REG_TYPE IP ETC*/
+  let dataListToWrite = dataList.map(item => item.includes(req.body.hostname) ? req.body.hostname+"\tIN\tA\t"+req.body.ip: item )
+  //console.log(dataListToWrite)
+
+  if(dataListToWrite.indexOf('') !== -1)
+  {
+    arrayToTxtFile(dataListToWrite, './db/db.temp', err => {
     if(err) {
       console.error(err)
       return
       }
     })
 
-  fs.copyFile('./db/db.temp', '/etc/bind/zones/master/<YOUR_ZONE_FILENAME.DOMAIN>', (err) => {
-  if (err) throw err;
-  console.log('Zone config file was copied');
-  });
+    fs.copyFile('./db/db.temp', '/etc/bind/zones/master/<db.YOUR_DOMANIN>', (err) => {
+    if (err) throw err;
+    console.log('Zone config file was copy');
+    });
 
-  exec("/etc/init.d/bind9 restart", (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-  });
+
+    exec("/etc/init.d/bind9 restart", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+  }
+  else
+  {
+          console.log("no changes");
+  }
 
   });
 
